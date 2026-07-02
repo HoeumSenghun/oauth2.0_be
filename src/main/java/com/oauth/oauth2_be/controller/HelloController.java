1ead0bbc65652ae8647be81288879077bea07ea0
HoeumSenghun <hoeumsenghun369@gmail.com>
2026-07-02 16:11:11 +0700

    20260702-configuration endpoint test

---
+++ src/main/java/com/oauth/oauth2_be/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.oauth.oauth2_be.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "backend is running";
+    }
+}
